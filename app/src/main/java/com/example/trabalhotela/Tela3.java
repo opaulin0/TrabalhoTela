b3dfbd62f124a946c204fc0a391652ed283b057a
Felipe Coronato <felipe@example.com>
2026-04-10 16:27:16 -0300

    Fix resource errors and prepare screens

--- app/src/main/java/com/example/trabalhotela/Tela3.java
+++ app/src/main/java/com/example/trabalhotela/Tela3.java
@@ -15,7 +15,7 @@ public class Tela3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tela3), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
